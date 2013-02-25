5ab259d3b31d05a35accd0db57ae7e31ebfa6bbf
Adrian Schoenig <adrian.schoenig@gmail.com>
2013-02-25 18:14:07 +1100

    Fixed compiler warnings

--- Classes/MGSplitViewController.m
+++ Classes/MGSplitViewController.m
@@ -943,7 +943,7 @@
 	if (_viewControllers && [_viewControllers count] > 0) {
 		NSObject *controller = [_viewControllers objectAtIndex:0];
 		if ([controller isKindOfClass:[UIViewController class]]) {
-			return [[controller retain] autorelease];
+			return [[(UIViewController *)controller retain] autorelease];
 		}
 	}
 	
@@ -985,7 +985,7 @@
 	if (_viewControllers && [_viewControllers count] > 1) {
 		NSObject *controller = [_viewControllers objectAtIndex:1];
 		if ([controller isKindOfClass:[UIViewController class]]) {
-			return [[controller retain] autorelease];
+			return [[(UIViewController *) controller retain] autorelease];
 		}
 	}
 	
